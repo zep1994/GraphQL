09bf9ea9b1505b436acfa39eef326778d936cd30
zep1994 <snaxbags@protonmail.com>
2019-03-10 21:25:48 -0700

    Export Root Query

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const graphHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express()
 
 app.use('/graphql', graphHTTP({
-    
+    schema
 }))
 
 app.listen(4000, () => {
