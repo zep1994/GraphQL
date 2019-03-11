79d4af048554f63866f8fbc0a83f696e62a8dd94
zep1994 <snaxbags@protonmail.com>
2019-03-10 21:38:07 -0700

    Add lodash

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,13 @@ const graphql = require('graphql')
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = 'graphql'
 
+//dummy data
+var books = [
+    { 'name': 'Name of the Wind', 'genre': 'Fantasy', 'id': '1'},
+    { 'name': 'Gone With the Wind', 'genre': 'Action', 'id': '2'},
+    { 'name': 'Rotten Tomatoes is rotten tomatoes', 'genre': 'Shitty Website', 'id': '3'}
+]
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
@@ -18,7 +25,7 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,
             args: {id: {type: GraphQLString}},
             resolve(parent, args){
-                //code to get date from db \ other source
+                
             }
         }
     }     
