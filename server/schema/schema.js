de9c29079709d0ce861c51a193fb9d89ba1f7711
zep1994 <snaxbags@protonmail.com>
2019-03-11 17:38:29 -0700

    Add author to schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,11 @@
 const graphql = require('graphql')
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
+const { GraphQLObjectType, 
+        GraphQLString, 
+        GraphQLSchema,
+        GraphQLID,
+        GraphQLInt } = graphql
 
 //dummy data
 var books = [
@@ -10,12 +14,27 @@ var books = [
     { 'name': 'Rotten Tomatoes is rotten tomatoes', 'genre': 'Shitty Website', 'id': '3'}
 ]
 
+var books = [
+    { 'name': 'me', age: 24, id: '1'},
+    { 'name': 'still me', age: 24, id: '2'},
+    { 'name': 'always me', age: 24, id: '3'},
+]
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
+    })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt}
     })
 })
 
